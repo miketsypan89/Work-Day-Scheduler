42c8f5ee2516666040fcd0c4d334f2c3ab00bb77
Mike Tsypan <miket@victorytransport.com>
2023-03-27 15:12:49 -0400

    finalized the js file

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 
 $(document).ready(function () {
   // console.log('testing')
@@ -11,21 +8,33 @@ $(document).ready(function () {
     console.log("time", time);
     localStorage.setItem(time, value);
   })
-  $('#hour-9 .description').val(localStorage.getItem('hour-9'));
-  $('#hour-10 .description').val(localStorage.getItem('hour-10'));
-  $('#hour-11 .description').val(localStorage.getItem('hour-11'));
-  $('#hour-12 .description').val(localStorage.getItem('hour-12'));
-  $('#hour-13 .description').val(localStorage.getItem('hour-13'));
-  $('#hour-14 .description').val(localStorage.getItem('hour-14'));
-  $('#hour-15 .description').val(localStorage.getItem('hour-15'));
-  $('#hour-16 .description').val(localStorage.getItem('hour-16'));
-  $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+  $('#9 .description').val(localStorage.getItem('9'));
+  $('#10 .description').val(localStorage.getItem('10'));
+  $('#11 .description').val(localStorage.getItem('11'));
+  $('#12 .description').val(localStorage.getItem('12'));
+  $('#13 .description').val(localStorage.getItem('13'));
+  $('#14 .description').val(localStorage.getItem('14'));
+  $('#15 .description').val(localStorage.getItem('15'));
+  $('#16 .description').val(localStorage.getItem('16'));
+  $('#17 .description').val(localStorage.getItem('17'));
 
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
 
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
+  // We'll create a for loop that will run and compare each hour with current hour and add or remove the necessary class
 
-  // TODO: Add code to display the current date in the header of the page.
+  for (let i = 9; i < 17; i++) {
+    if (dayjs().hour() === i) {
+      $('#' + i).removeClass('past future');
+      $('#' + i).addClass('present');
+    } else if (dayjs().hour() > i) {
+      $('#' + i).addClass('past');
+      $('#' + i).removeClass('present future');
+    } else {
+      $('#' + i).addClass('future');
+      $('#' + i).removeClass('present past');
+    }
+  }
+
+  // We'll display the current date in the header of the page.
 
   Date.prototype.yyyymmdd = function () {
     var yyyy = this.getFullYear().toString();
@@ -34,7 +43,8 @@ $(document).ready(function () {
     return (mm[1] ? mm : "0" + mm[0]) + "/" + (dd[1] ? dd : "0" + dd[0]) + "/" + yyyy;
   };
 
-  var currentDay = new Date();
-  localStorage.setItem('date, value');
+  var currentDay = dayjs().format('dddd, MMMM D, YYYY h:mm A')
+  $('#currentDay').text(currentDay);
+
   $('#currentDay .lead').val(localStorage.getItem(value));
 });
